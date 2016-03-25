f2a44152b75ad613d7d21f0f9ad4e698f3205dc8
thphong <duongthanhphong79@gmail.com>
2016-03-25 23:24:55 +0700

    Ok

--- StoreManagement.Website/App_Start/BundleConfig.cs
+++ StoreManagement.Website/App_Start/BundleConfig.cs
@@ -12,7 +12,7 @@ namespace StoreManagement.Website
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
@@ -25,19 +25,8 @@ namespace StoreManagement.Website
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/FValidation.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/jqueryui").Include(
+                        "~/Content/jquery-ui.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
                         "~/Scripts/angular.js"));
--- StoreManagement.Website/Scripts/Controller/Order.js
+++ StoreManagement.Website/Scripts/Controller/Order.js
@@ -1,5 +1,36 @@
 ﻿$(document).ready(function () {
     $('input.datepicker').datepicker({ format: 'dd-mm-yyyy'/*, startDate: '23-03-2016'*/ });
+
+    $('input[ng-model="OrderForm.Customer"]').autocomplete({
+        minLength: 0,
+        source: function (request, response) {
+
+            var configList = new GridViewConfig("");
+            configList.GridDataAction = "get10";
+            configList.GridDataObject = "T_Master_Customers";
+            configList.GridDefinedColums = "CustomerId;CustomerCode;CustomerName";
+            configList.GridSortCondition = "[CustomerCode] ASC";
+            configList.GridFilterCondition = "[IsActive] = 1 and ([CustomerCode] like N''%" + request.term + "%'' or [CustomerName] like N''%" + request.term + "%'')";
+
+            var listData = configList.GetListData();
+            response(listData);
+        },
+        focus: function (event, ui) {
+            $('input[ng-model="OrderForm.Customer"]').val(ui.item["CustomerName"]);
+            $("#project-id").val(ui.item.value);
+            return false;
+        },
+        select: function (event, ui) {
+            $('input[ng-model="OrderForm.Customer"]').val(ui.item["CustomerName"]);
+            $("#project-id").val(ui.item.value);
+            return false;
+        }
+    })
+    .autocomplete("instance")._renderItem = function (ul, item) {
+        return $("<li>")
+                .append("<a> <b>" + item["CustomerCode"] + " </b><br>" + item["CustomerName"] + "</a>")
+                .appendTo(ul);
+     };
 })
 
 
@@ -47,7 +78,7 @@ mdlCommon.controller('OrderController',
                     $scope.AdditionalFilter.EndDate = formatDate(new Date(y, m, 0));
                     break;
                 case 6: //Last quarter
-                    $scope.AdditionalFilter.StartDate = formatDate(new Date(y, (quarter -1) * 3, 1));
+                    $scope.AdditionalFilter.StartDate = formatDate(new Date(y, (quarter - 1) * 3, 1));
                     $scope.AdditionalFilter.EndDate = formatDate(new Date(y, quarter * 3, 0));
                     break;
             }
@@ -70,11 +101,15 @@ mdlCommon.controller('OrderController',
         $scope.IsEditingOrderDetail = false;
 
         $scope.OrderForm = {
-            OrderId: "-1"
+            OrderId: "-1",
+            OrderCode: "",
+            Customer: ""
         };
 
         $scope.ResetOrderForm = function () {
             $scope.OrderForm.OrderId = "-1";
+            $scope.OrderForm.OrderCode = "";
+            $scope.OrderForm.Customer = "";
         }
 
         $scope.AddOrder = function () {
@@ -82,6 +117,8 @@ mdlCommon.controller('OrderController',
             $scope.IsEditingOrderDetail = true;
             FValidation.ClearAllError();
             $scope.ResetOrderForm();
+
+            
         }
 
         $scope.CloseOrderDetail = function () {
