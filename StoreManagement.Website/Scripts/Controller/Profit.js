8ebb32ac9800680e2936990564744574e8a43054
thphong <duongthanhphong79@gmail.com>
2016-05-02 07:01:51 +0700

    Profit

--- StoreManagement.Website/Scripts/Controller/Profit.js
+++ StoreManagement.Website/Scripts/Controller/Profit.js
@@ -13,7 +13,8 @@
 
         $scope.Filter = {
             ReportGroupBy: "d",
-            CurrentStore: "0"
+            CurrentStore: "0",
+            ProductGroup: "0"
         };
 
         $scope.BarChartByTime = null;
@@ -54,6 +55,86 @@
                 listName.push(listData[i].GroupBy);
             }
             $scope.BindBarChartByTime(cost, profit, listName);
+
+        }, false);
+
+        $scope.BarChartByProduct = null;
+        $scope.BindBarChartByProduct = function (cost, profit, listName) {
+            setTimeout(function () {
+                if ($scope.BarChartByProduct == null) {
+                    $scope.BarChartByProduct = c3.generate({
+                        bindto: '#chartReportByProduct',
+                        data: { x: 'x', columns: [], type: "bar" },
+                        axis: {
+                            x: { label: 'Sản phẩm', type: 'category' } //this needed to load string x value
+                            , y: { label: 'Doanh thu' }
+                        },
+                        bar: { width: { ratio: 0.5 } },
+                        legend: { show: false }
+                    });
+                }
+                if (listName.length > 1) {
+                    $scope.BarChartByProduct.load({
+                        columns: [listName, cost, profit]
+                    });
+                }
+                else {
+                    $scope.BarChartByProduct.unload();
+                }
+            }, 10);
+        }
+
+        $scope.$watch('DataSet.ReportByProduct.Data', function (newVal, oldVal) {
+            var listData = newVal;
+            var listName = ['x'];
+            var cost = ['Vốn'];
+            var profit = ['Lãi'];
+            for (var i = 0; i < listData.length; i++) {
+                cost.push(listData[i].Cost);
+                profit.push(listData[i].Profit);
+                listName.push(listData[i].ProductCode);
+            }
+            $scope.BindBarChartByProduct(cost, profit, listName);
+
+        }, false);
+
+        $scope.BarChartByStore = null;
+        $scope.BindBarChartByStore = function (cost, profit, listName) {
+            setTimeout(function () {
+                if ($scope.BarChartByStore == null) {
+                    $scope.BarChartByStore = c3.generate({
+                        bindto: '#chartReportByStore',
+                        data: { x: 'x', columns: [], type: "bar" },
+                        axis: {
+                            x: { label: 'Cửa hàng', type: 'category' } //this needed to load string x value
+                            , y: { label: 'Doanh thu' }
+                        },
+                        bar: { width: { ratio: 0.5 } },
+                        legend: { show: false }
+                    });
+                }
+                if (listName.length > 1) {
+                    $scope.BarChartByStore.load({
+                        columns: [listName, cost, profit]
+                    });
+                }
+                else {
+                    $scope.BarChartByStore.unload();
+                }
+            }, 10);
+        }
+
+        $scope.$watch('DataSet.ReportByStore.Data', function (newVal, oldVal) {
+            var listData = newVal;
+            var listName = ['x'];
+            var cost = ['Vốn'];
+            var profit = ['Lãi'];
+            for (var i = 0; i < listData.length; i++) {
+                cost.push(listData[i].Cost);
+                profit.push(listData[i].Profit);
+                listName.push(listData[i].StoreCode);
+            }
+            $scope.BindBarChartByStore(cost, profit, listName);
 
         }, false);
 
