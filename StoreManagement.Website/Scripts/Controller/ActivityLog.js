7fdbbb9ef7bfd746ab54f5f1bdb98a41ded728e8
thphong <duongthanhphong79@gmail.com>
2016-11-29 19:23:25 +0700

    Add file

---
+++ StoreManagement.Website/Scripts/Controller/ActivityLog.js
@@ -0,0 +1,12 @@
+﻿mdlCommon.controller('ActivityController',
+['$scope', '$filter', '$controller',
+    function ($scope, $filter, $controller) {
+        $controller('ctrlPaging', { $scope: $scope });
+
+        $scope.AdditionalFilter = {
+            Action: "0"
+        };
+
+
+        //test
+    }]);
