28d1df80bf11a3a9132f64fe6b407615b9840a6a
chumano <xuanloc_hoang2000@yahoo.com>
2016-12-16 11:56:56 +0700

    Add files via upload

---
+++ StoreManagement.Website/Scripts/jquery.ui.autocomplete.selectFirst.js
@@ -0,0 +1,34 @@
+/*
+ * jQuery UI Autocomplete Select First Extension
+ *
+ * Copyright 2010, Scott González (http://scottgonzalez.com)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * http://github.com/scottgonzalez/jquery-ui-extensions
+ */
+jQuery.fn.extend({
+    live: function (event, callback) {
+        if (this.selector) {
+            jQuery(document).on(event, this.selector, callback);
+
+        }
+    }
+});
+
+(function( $ ) {
+    
+
+$( ".ui-autocomplete-input" ).live( "autocompleteopen", function() {
+    var autocomplete = $(this).autocomplete();
+    /*
+	var	menu = autocomplete.menu;
+
+	if ( !autocomplete.options.selectFirst ) {
+		return;
+	}
+
+	menu.activate( $.Event({ type: "mouseenter" }), menu.element.children().first() );
+    */
+});
+
+}( jQuery ));
