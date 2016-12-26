58fa0fee735ca3d6f44780ee96e14b29367caeb2
thphong <duongthanhphong79@gmail.com>
2016-12-26 11:05:57 +0700

    Add files

---
+++ StoreManagement.Website/Scripts/Controller/ManageAccount.js
@@ -0,0 +1,26 @@
+﻿mdlCommon.controller('ManageStoreController',
+['$scope', '$filter', '$controller', '$interpolate', '$sce',
+    function ($scope, $filter, $controller, $interpolate, $sce) {
+        $controller('ctrlPaging', { $scope: $scope });
+
+        //General info
+        $scope.AccountInfo = {
+            AvailableAmount: "",
+            UsedAmount: "",
+            ChargedAmount: ""
+        }
+        
+        $scope.SelectedMonth = -1;
+        $scope.SelectedYear = -1;
+
+        $scope.AccountFormConfig = new ObjectDataConfig("T_Trans_StoreAccount", $scope);
+        var object = $scope.AccountFormConfig.GetObject($scope.ParentStore);
+        $scope.AccountFormConfig.CopyFields(object, $scope.AccountInfo);
+
+        $scope.SelectMonth = function(item) 
+        {
+            $scope.SelectedMonth = item.Month;
+            $scope.SelectedYear = item.Year;
+        }
+
+    }]);
