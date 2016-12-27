b915ee932c9cd23d6e03cfcaf2d8f4a11a280e9e
chumano <xuanloc_hoang2000@yahoo.com>
2016-12-27 21:42:17 +0700

    delete bin folders

--- StoreManagement.Website/obj/Release/Package/PackageTmp/Scripts/masterpage.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    var pathname = window.location.pathname;
-    $("#nav-list-menu li").each(function () {
-        if ($(this).children("a[href=" + pathname + "]").length > 0) {
-            $(this).addClass("active");
-        }
-    });
-});
